ed2ed96527cc160082a9b4beed1340dc0ec4def0
Layton Wang <wangzixu.china@gmail.com>
2017-07-20 13:12:27 +0800

    Include ThreadPool from module XNRW, finish server

--- server.cpp
+++ server.cpp
@@ -3,27 +3,48 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
+#include <cstddef>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include "XNRW/include/ThreadPool.h"
+
 int main(int argc, char const *argv[]) {
-  if (argc < 2) {
-    std::cerr << "Usage: server <message size in bytes>" << std::endl;
+  if (argc < 3) {
+    std::cerr << "Usage: server <number of connections> <message size in bytes>"
+      << std::endl;
     return -1;
   }
-  unsigned long long messageSize = std::stoull(argv[1], nullptr, 10);
+  const auto messageSize = std::stoull(argv[1], nullptr, 10);
+  const auto numConn = std::stoull(argv[2], nullptr, 10);
+  const auto reply = "ACK";
+
+  XNRW::ThreadPool threadPool(numConn);
+
   int serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  struct sockaddr_in serverAddress;
+  struct sockaddr_in serverAddress, clientAddress;
   memset(&serverAddress, 0, sizeof(serverAddress));
   serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
   serverAddress.sin_port = htons(1234);
+  socklen_t clientLen = sizeof(clientAddress);
   bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+  listen(serverSocket, numConn);
 
-  while (true) {
-    //
+  for (auto i = 0; i < numConn; i++) {
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress,
+                              &clientLen);
+    threadPool.addTask([clientSocket, messageSize, reply]() {
+      std::vector<unsigned char> buffer;
+      read(clientSocket, buffer.data(), messageSize);
+      write(clientSocket, reply, sizeof(reply));
+      close(clientSocket);
+    });
   }
+
+  threadPool.wait();
+
   return 0;
 }
